0431533abafe1c33de7d712afaa8d3a8a52f86f9
acooo1 <antoine.corne@free.fr>
2023-11-13 19:38:59 +0100

    First framer test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { motion } from 'framer-motion';
+
 import reactLogo from './assets/react.svg';
 
 function App() {
   return (
     <main className='flex h-full w-full flex-col items-center justify-center'>
+
       <section className='flex flex-col items-center'>
-        <img src={reactLogo} className='h-24 w-24' alt='React logo' />
-        <h2 className='text-3xl'>Vite + React + TypeScript + Tailwind</h2>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2}}
+          whileHover={{ scale: 1.2 }}
+        >
+          <img src={reactLogo} className='h-24 w-24' alt='React logo' />
+        </motion.div>
+        <p>Hover me !</p>
       </section>
+        
     </main>
   );
 }
